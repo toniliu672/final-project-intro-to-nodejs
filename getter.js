d0dfe0eac4949fe688920cd181273a66f1ef334d
Toni Liungan <toniliu672@gmail.com>
2022-12-17 02:21:42 +0800

    first commit

---
+++ getter.js
@@ -0,0 +1,97 @@
+function mapUsers(users) {
+  return {
+    data: users, 
+    count: users.length
+  }
+}
+
+function mapArticles(articles) {
+  return{
+    data: articles,
+    count: articles.length
+  }
+}
+
+const users = [
+  {
+    "id": 1,
+    "username": "isrotrip",
+    "password": "PhT1Oky01",
+    "gender": "male"
+  },
+  {
+    "id": 2,
+    "username": "dmtrxw",
+    "password": "oYt2Thj9w",
+    "gender": "male"
+  },
+  {
+    "id": 3,
+    "username": "afifahrahmak",
+    "password": "YhI6ErG71",
+    "gender": "female"
+  },
+  {
+    "id": 4,
+    "username": "ijtj",
+    "password": "cok1BgS77",
+    "gender": "male"
+  },
+  {
+    "id": 5,
+    "username": "arnoltherigan",
+    "password": "Ko4Rn0lD",
+    "gender": "male"
+  },
+]
+
+const articles = [
+  {
+    "id": 1,
+    "article": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "userId": 1
+  },
+  {
+    "id": 2,
+    "article": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "userId": 2
+  },
+  {
+    "id": 3,
+    "article": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "userId": 3
+  },
+  {
+    "id": 4,
+    "article": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "userId": 4
+  },
+  {
+    "id": 5,
+    "article": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "userId": 1
+  },
+  {
+    "id": 6,
+    "article": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "userId": 2
+  },
+  {
+    "id": 7,
+    "article": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "userId": 3
+  },
+  {
+    "id": 8,
+    "article": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "userId": 5
+  },
+]
+
+console.log(mapUsers(users))
+console.log(mapArticles(articles))
+
+export default mapUsers
+export {
+  mapArticles 
+} 
